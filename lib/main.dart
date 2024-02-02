b6a9d0a4aca7ebe4359697400ef9692835497097
jahanzeb001 <zebi.biit@gmail.com>
2024-02-02 17:53:59 +0500

    code push to remote repo

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,8 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: false,
         ),
+
+        /////////////////
         home: const ResponsiveLayout());
   }
 }
